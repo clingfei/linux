03bb487fd4edff905e8fbee84a012ebeda12549f
Cheng Lingfei <chenglingfei@foxmail.com>
2026-07-15 23:40:48 +0800

    lkl: Add cross-platform ELF definitions
    Add ELF_DATA and ELF_ARCH definitions to support multiple host
    architectures: x86_64, i386, aarch64, arm, riscv, and s390.

--- arch/lkl/include/asm/elf.h
+++ arch/lkl/include/asm/elf.h
@@ -9,6 +9,28 @@
 #define ELF_CLASS ELFCLASS32
 #endif
 
+#ifdef CONFIG_CPU_BIG_ENDIAN
+#define ELF_DATA ELFDATA2MSB
+#else
+#define ELF_DATA ELFDATA2LSB
+#endif
+
+#if defined(__x86_64__)
+#define ELF_ARCH EM_X86_64
+#elif defined(__i386__)
+#define ELF_ARCH EM_386
+#elif defined(__aarch64__)
+#define ELF_ARCH EM_AARCH64
+#elif defined(__arm__)
+#define ELF_ARCH EM_ARM
+#elif defined(__riscv)
+#define ELF_ARCH EM_RISCV
+#elif defined(__s390__)
+#define ELF_ARCH EM_S390
+#else
+#error "Unsupported LKL ELF target"
+#endif
+
 #ifdef CONFIG_MMU
 #define ELF_EXEC_PAGESIZE 4096
 #define ELF_PLATFORM "i586"
@@ -19,4 +41,3 @@
 #define elf_gregset_t long
 #define elf_fpregset_t double
 #endif
-
